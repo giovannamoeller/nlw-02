4388536757cf4529e75f4600f3fcce45e41dd93d
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-08-04 21:38:14 -0300

    Criando a página de formulário

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../assets/styles/global.css';
+import './style.css';
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return(
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer dar aulas."/>
+        </div>
+    )
+}
+
+export default TeacherForm;
