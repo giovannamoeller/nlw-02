98b166c9258d328b254f7d38e31fa091eb303fca
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-08-04 21:37:23 -0300

    Criando página inicial e componente

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -21,15 +22,15 @@ const Landing = () => {
                 className="hero-image"/>
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="" className="study">
+                    <Link to="/give-classes" href="" className="study">
                         <img src={giveClassesIcon} alt="Estudar"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
