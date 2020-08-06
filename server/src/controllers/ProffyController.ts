f8265739c008ad62238a4c685728cf99987e6da5
Giovanna Moeller <giovannamoeller@MacBook-Air-de-Giovanna.local>
2020-08-06 10:55:39 -0300

    Finalizando API

--- server/src/controllers/ProffyController.ts
+++ server/src/controllers/ProffyController.ts
@@ -9,7 +9,7 @@ interface ScheduleItem {
 }
 
 const Proffy = {
-  async createProffy(req: Request, res: Response) {
+  async create(req: Request, res: Response) {
     const {
       name,
       avatar_url,
@@ -55,8 +55,29 @@ const Proffy = {
     }
   },
 
-  async getProffys(req: Request, res: Response) {
-      
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    if(!filters.week_day || !filters.subject || !filters.time) {
+        return res.status(400).json({
+            error: 'Missing filters'
+        });
+    }
+
+    const timeInMinutes = convertHourToMinutes(filters.time as string);
+
+    const classes = await knex('proffys')
+        .whereExists(function() {
+            this.select('schedule.*')
+                .from('schedule')
+                .whereRaw('schedule.user_id = proffys.id')
+                .whereRaw('schedule.week_day = ??', [Number(filters.week_day)])
+                .whereRaw('schedule.from <= ??', [timeInMinutes])
+                .whereRaw('schedule.to > ??', [timeInMinutes])
+        })
+        .where('proffys.subject', '=', filters.subject as string)
+
+    return res.json(classes);
   }
 };
 
